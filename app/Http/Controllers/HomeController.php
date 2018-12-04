1a99cca75ddbcf861085fc635778540792b50c75
oammike <mpamero@openaccessbpo.com>
2018-12-04 11:18:39 +0800

    separated postmate log file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -489,7 +489,7 @@ class HomeController extends Controller
                       $user = User::find(Input::get('id'));
                       $formID = Input::get('formid');
                       $usersubmit = Input::get('usersubmit');
-                      $file = fopen('public/build/changes.txt', 'a') or die("Unable to open logs");
+                      $file = fopen('public/build/postmatelogs.txt', 'a') or die("Unable to open logs");
                         fwrite($file, "-------------------\n Submitted Form [".$formID."][".$usersubmit."] via tab: " . $correct->format('M d h:i A'). " by [". $this->user->id."] ".$this->user->lastname."\n");
                         fclose($file);
                     } 
@@ -499,7 +499,7 @@ class HomeController extends Controller
         case '5':{
                     if($this->user->id !== 564 ) {
                       $user = User::find(Input::get('id'));
-                      $file = fopen('public/build/changes.txt', 'a') or die("Unable to open logs");
+                      $file = fopen('public/build/postmatelogs.txt', 'a') or die("Unable to open logs");
                         fwrite($file, "-------------------\n Played Video by [". $this->user->id."] ".$this->user->lastname." on ". $correct->format('M d h:i A').  "\n");
                         fclose($file);
                     } 
