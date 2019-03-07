62391ecc97319109a7c48a35c0b5f748e4605b55
oammike <mpamero@openaccessbpo.com>
2019-03-07 12:09:40 +0800

    fix notif errors

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -118,9 +118,9 @@ class NotificationController extends Controller
                         case '6': $fromData =ImmediateHead::find(ImmediateHead_Campaign::find(User_CWS::find($notif->detail->relatedModelID)->approver)->immediateHead_id);break;
                         case '7': { 
                                     $ih = ImmediateHead_Campaign::find(User_OT::find($notif->detail->relatedModelID)->approver);
-                                    if (count() > 0 ){
+                                    if ( count() > 0 ){
 
-                                      $fromData =ImmediateHead::find($ih->immediateHead_id);}break;
+                                      $fromData =ImmediateHead::find($ih->immediateHead_id);
                                     }else{
                                       $fromData = User::find(User_OT::find($notif->detail->relatedModelID)->approver)->id;
 
