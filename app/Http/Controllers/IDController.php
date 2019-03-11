1a9093797a7a649df31f4448a19b700f13cd5174
Lester Bambico <lockzackary@gmail.com>
2019-03-11 14:27:13 +0800

    changed permission requirement from PRINT_ID to EDIT_EMPLOYEE

--- app/Http/Controllers/IDController.php
+++ app/Http/Controllers/IDController.php
@@ -19,7 +19,7 @@ class IDController extends Controller
         $this->user =  User::find(Auth::user()->id);
         $this->url = $url;
         $this->campaign_mode = false;
-        $print_id = UserType::find($this->user->userType_id)->roles->where('label','PRINT_ID');
+        $print_id = UserType::find($this->user->userType_id)->roles->where('label','EDIT_EMPLOYEE');
         $this->has_id_permissions =  !$print_id->isEmpty();
         
         /*
