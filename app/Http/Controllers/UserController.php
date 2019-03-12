5b034609ac525dc3efb9315ebf03c1a6369fa13e
oammike <mpamero@openaccessbpo.com>
2019-03-12 19:46:47 +0800

    log crop

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2592,6 +2592,15 @@ class UserController extends Controller
     {
       $user = User::find($id);
 
+      //return $inactiveUsers1;
+        $correct = Carbon::now('GMT+8');
+
+        if($this->user->id !== 564 ) {
+            $file = fopen('public/build/changes.txt', 'a') or die("Unable to open logs");
+            fwrite($file, "-------------------\n Cropped [".$id."] on " . $correct->format('M d h:i A'). " by [". $this->user->id."] ".$this->user->lastname."\n");
+            fclose($file);
+        }
+
       if (count($user)>1){
         $img = storage_path().'/uploads/id/'.$user->id.'_portrait.png';
         $imgfile = '../storage/uploads/id/'.$user->id.'_portrait.png';
