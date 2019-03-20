7035d7666590a921aa574916dbcd3e85bc0ad3ef
oammike <mpamero@openaccessbpo.com>
2019-03-20 13:57:05 +0800

    updated video

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -216,6 +216,23 @@ select:-webkit-autofill:focus {
 
               <div class="col-lg-5 col-sm-6 col-xs-12">
 
+                <!--VIDEOS -->
+                <div class="box box-info" style="background: rgba(256, 256, 256, 0.6)">
+                    <div class="box-header with-border">
+                      <h3 class="box-title">Videos</h3>
+                      <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                        <!-- <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button> -->
+                      </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                      <p class="text-center"><strong class="text-primary">Let's Get Physical <br/><span class="text-orange">Yoga | Aero Kickboxing | Zumba</span>  </strong></p>
+                      <video id="teaser" src="storage/uploads/getphysical.webm" width="100%" loop controls></video>
+                    </div>
+                </div>
+
                 @if ($doneSurvey == 0)
                 <!--EES -->
                 <div class="box box-info" style="background: rgba(256, 256, 256, 0.6)">
@@ -380,27 +397,7 @@ select:-webkit-autofill:focus {
                   </div>
                 <!-- /.end SHOUT OUT -->
 
-                <!--VIDEOS -->
-                <div class="box box-info" style="background: rgba(256, 256, 256, 0.6)">
-                    <div class="box-header with-border">
-                      <h3 class="box-title">Videos</h3>
-
-                      <div class="box-tools pull-right">
-                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                        </button>
-                        <!-- <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button> -->
-                      </div>
-                    </div>
-                    <!-- /.box-header -->
-                    <div class="box-body">
-                      <p class="text-center"> <strong class="text-primary">Let's Get Physical: </strong> <strong>Zumba</strong> Sessions [03.07.2019] <br/>  </strong> 
-                               </p>
-
-                                
-                              
-                              <video id="teaser" src="storage/uploads/zumba2.webm" width="100%" loop controls></video>
-                    </div>
-                  </div>
+                
 
 
 
