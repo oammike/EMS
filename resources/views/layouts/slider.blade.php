a40b91ef284b0a8207518b26218cabfea4c4a255
Mike <mpamero@openaccessbpo.com>
2019-10-03 12:53:36 +0800

    updated slider

--- resources/views/layouts/slider.blade.php
+++ resources/views/layouts/slider.blade.php
@@ -23,27 +23,7 @@
 
                   </div>
 
-                  <div class="item  text-center" style="background-color: #fff" >
-                      
-                              <!-- <h4 class="text-orange" >Monday <span class="text-primary"> Motivation </span></h4> -->
-                              <img src="./storage/uploads/heartday-1.jpg" style="z-index: 2" />
-                               <p class="text-center" style="padding: 30px;"><br/><br/><strong class="text-primary">Sept.29 : Happy World Heart Day!</strong>
-                                <br/><br/>Every heartbeat matters. On #WorldHeartDay, make the commitment to take care of your heart and prevent heart diseases and stroke. Inspire your loved ones to do the same!<br/><br/>#WeSpeakYourLanguage #OAonHeartDay</p>
-
-                              <br/><br/>
-
-                  </div>
-                  <div class="item  text-center" style="background-color: #fff" >
-                      
-                              <!-- <h4 class="text-orange" >Monday <span class="text-primary"> Motivation </span></h4> -->
-                              <img src="./storage/uploads/heartday-2.jpg" style="z-index: 2" />
-                               <p class="text-center" style="padding: 30px;"><br/><br/><strong class="text-primary">Sept.29 : Happy World Heart Day!</strong>
-                                <br/><br/>Every heartbeat matters. On #WorldHeartDay, make the commitment to take care of your heart and prevent heart diseases and stroke. Inspire your loved ones to do the same!<br/><br/>#WeSpeakYourLanguage #OAonHeartDay</p>
-
-                              <br/><br/>
-
-                  </div>
-
+                 
                       <!-- BLOOD DONATION-->
                 <div class="item text-center" >
                     
@@ -68,68 +48,8 @@
                       <h5 class="text-primary"> Every volunteer donor will be given a BLOOD DONOR CARD during the event. This card may be used as a record of donation. However, this card does not exempt the holder from paying the processing fee. This is intended to cover the cost of the reagents an operating expenses used to collect and screen all donated blood for infectious disease</h5><br/><br/><br/><br/>
                 </div>
 
-                <!--zumba sched-->
-                <div class="item text-center" >
-                    
-                     <p style="padding: 5px 30px; margin-bottom: 0px"><h3 class="text-orange"><span style="font-size: smaller;" class="text-primary">Dance Your Way</span> To Boracay!</h3>
-                     <img src="./storage/uploads/letsgetphysical-banner.jpg" style="z-index: 2" /><br/>
-                     <br/><br/>
-
-                     <strong style="font-size:larger"><span class="text-primary"> Tuesday:</span> 
-                        <br/><span class="text-danger">October 01, 2019 (Tue) – 4:30 PM</span><br/></strong><br/>
-
-                      <strong style="font-size:larger"><span class="text-primary"> Thursday:</span> 
-                        <BR/><span class="text-danger">October 03, 2019 (Thu) – 7:00 PM</span><br/></strong><br/> 
-
-                        <a class="btn btn-danger" href="https://docs.google.com/forms/d/e/1FAIpQLSeZsdfWT5UvVOWnWmEGG9uAhmfLLVyK1yHyJ9U-wP7KciTsPQ/viewform" target="_blank">
-                          Sign Up Now</a><br/><br/>
-
-                          <p class="text-left" style="padding-left: 30px;"><br/><br/>All attendees of the workout classes will be eligible to win in the raffle! For each class you attend, you'll have a raffle entry. The more classes you attend, the more chances you'll have to win the major prize! <br/><br/>Here are the amazing prizes up for grabs:</p>
-
-                          <h5 class="text-primary">Major Prize: <strong>1 Winner - Trip to Boracay!</strong></h5>
-                          
-                          <div class="text-left"  style="padding-left: 30px;">
-                            <h5>Minor Prizes:</h5>
-                            <ul>
-                              <li>3 winners: SM Gift certificates worth Php 2,000</li>
-                              <li>1 winner: SM Gift certificates worth Php 1,500</li>
-                              <li>1 winner: SM Gift certificates worth Php 1,000</li>
-                              <li>1 winner: JBL Flip3 Bluetooth speaker</li>
-                              <li>1 winner: Promate 10,000mAh Powerbank</li>
-                              <li>1 winner: urBeats3 earphones</li>
-                            </ul></div>
-
-                         
-
-                      <br/><br/>
-                       <strong>Venue:</strong><br/>
-
-                        OPEN ACCESS BPO G2 OFFICE<br/>
-
-                        11F Glorietta 2 Corporate Center,<br/>
-
-                        West. St., Ayala Center, Makati City<br/><br/>
-
-                        <img src="./storage/uploads/oam-location.jpg" style="z-index: 2" width="100%" />
-                        <br/><br/>
-
-                          View our <a class="text-danger" href="./gallery?a=5"><i class="fa fa-picture-o"></i> Gallery</a> for more<br/> #WeSpeakYourLanguage #LetsGetPhysical #OAforWellness
-
-                       
-                        <div style="padding:10px; position: absolute;bottom: 0px; right: 0px; background: rgba(0, 0, 0, 0.8)"> <a style="color:#fff" href="https://www.instagram.com/openaccessbpo/" target="_blank" title="Follow us on Instagram!">
-                              <small>Follow us on Instagram! <strong>@openaccessbpo</strong> <br/> #WeSpeakYourLanguage #OAforWellness #LetsGetPhysical #Zumba</small></a></div> <br/><br/><br/><br/>
-                </div> 
-
-                  <div class="item  text-center" style="background-color: #fff" >
-                      
-                              <!-- <h4 class="text-orange" >Monday <span class="text-primary"> Motivation </span></h4> -->
-                              <img src="./storage/uploads/rosh.jpg" style="z-index: 2" />
-                               <p class="text-center" style="padding: 30px;"><br/><br/><strong class="text-primary">Shanah Tovah</strong>
-                                <br/><br/>Shanah Tovah to our Jewish brothers and sisters celebrating #RoshHashanah. It also means “Head of the Year” and serves as the Jewish New Year, marking the first of the High Holidays in their calendar.<br/><br/>#WeSpeakYourLanguage #OAonRoshHashanah</p>
-
-                              <br/><br/>
-
-                  </div>
+               
+                  
 
                   <div class="item text-center" style="background-color: #fff" >
                     <img src="./storage/uploads/blacknwhite.jpg" style="z-index: 2" />
@@ -562,6 +482,93 @@
 
 
 <?php /*
+
+<!-- shana tova -->
+<div class="item  text-center" style="background-color: #fff" >
+                      
+                              <!-- <h4 class="text-orange" >Monday <span class="text-primary"> Motivation </span></h4> -->
+                              <img src="./storage/uploads/rosh.jpg" style="z-index: 2" />
+                               <p class="text-center" style="padding: 30px;"><br/><br/><strong class="text-primary">Shanah Tovah</strong>
+                                <br/><br/>Shanah Tovah to our Jewish brothers and sisters celebrating #RoshHashanah. It also means “Head of the Year” and serves as the Jewish New Year, marking the first of the High Holidays in their calendar.<br/><br/>#WeSpeakYourLanguage #OAonRoshHashanah</p>
+
+                              <br/><br/>
+
+                  </div>
+ <!--zumba sched-->
+                <div class="item text-center" >
+                    
+                     <p style="padding: 5px 30px; margin-bottom: 0px"><h3 class="text-orange"><span style="font-size: smaller;" class="text-primary">Dance Your Way</span> To Boracay!</h3>
+                     <img src="./storage/uploads/letsgetphysical-banner.jpg" style="z-index: 2" /><br/>
+                     <br/><br/>
+
+                     <strong style="font-size:larger"><span class="text-primary"> Tuesday:</span> 
+                        <br/><span class="text-danger">October 01, 2019 (Tue) – 4:30 PM</span><br/></strong><br/>
+
+                      <strong style="font-size:larger"><span class="text-primary"> Thursday:</span> 
+                        <BR/><span class="text-danger">October 03, 2019 (Thu) – 7:00 PM</span><br/></strong><br/> 
+
+                        <a class="btn btn-danger" href="https://docs.google.com/forms/d/e/1FAIpQLSeZsdfWT5UvVOWnWmEGG9uAhmfLLVyK1yHyJ9U-wP7KciTsPQ/viewform" target="_blank">
+                          Sign Up Now</a><br/><br/>
+
+                          <p class="text-left" style="padding-left: 30px;"><br/><br/>All attendees of the workout classes will be eligible to win in the raffle! For each class you attend, you'll have a raffle entry. The more classes you attend, the more chances you'll have to win the major prize! <br/><br/>Here are the amazing prizes up for grabs:</p>
+
+                          <h5 class="text-primary">Major Prize: <strong>1 Winner - Trip to Boracay!</strong></h5>
+                          
+                          <div class="text-left"  style="padding-left: 30px;">
+                            <h5>Minor Prizes:</h5>
+                            <ul>
+                              <li>3 winners: SM Gift certificates worth Php 2,000</li>
+                              <li>1 winner: SM Gift certificates worth Php 1,500</li>
+                              <li>1 winner: SM Gift certificates worth Php 1,000</li>
+                              <li>1 winner: JBL Flip3 Bluetooth speaker</li>
+                              <li>1 winner: Promate 10,000mAh Powerbank</li>
+                              <li>1 winner: urBeats3 earphones</li>
+                            </ul></div>
+
+                         
+
+                      <br/><br/>
+                       <strong>Venue:</strong><br/>
+
+                        OPEN ACCESS BPO G2 OFFICE<br/>
+
+                        11F Glorietta 2 Corporate Center,<br/>
+
+                        West. St., Ayala Center, Makati City<br/><br/>
+
+                        <img src="./storage/uploads/oam-location.jpg" style="z-index: 2" width="100%" />
+                        <br/><br/>
+
+                          View our <a class="text-danger" href="./gallery?a=5"><i class="fa fa-picture-o"></i> Gallery</a> for more<br/> #WeSpeakYourLanguage #LetsGetPhysical #OAforWellness
+
+                       
+                        <div style="padding:10px; position: absolute;bottom: 0px; right: 0px; background: rgba(0, 0, 0, 0.8)"> <a style="color:#fff" href="https://www.instagram.com/openaccessbpo/" target="_blank" title="Follow us on Instagram!">
+                              <small>Follow us on Instagram! <strong>@openaccessbpo</strong> <br/> #WeSpeakYourLanguage #OAforWellness #LetsGetPhysical #Zumba</small></a></div> <br/><br/><br/><br/>
+                </div> 
+
+
+<!-- heart day-->
+ <div class="item  text-center" style="background-color: #fff" >
+                      
+                              <!-- <h4 class="text-orange" >Monday <span class="text-primary"> Motivation </span></h4> -->
+                              <img src="./storage/uploads/heartday-1.jpg" style="z-index: 2" />
+                               <p class="text-center" style="padding: 30px;"><br/><br/><strong class="text-primary">Sept.29 : Happy World Heart Day!</strong>
+                                <br/><br/>Every heartbeat matters. On #WorldHeartDay, make the commitment to take care of your heart and prevent heart diseases and stroke. Inspire your loved ones to do the same!<br/><br/>#WeSpeakYourLanguage #OAonHeartDay</p>
+
+                              <br/><br/>
+
+                  </div>
+                  <div class="item  text-center" style="background-color: #fff" >
+                      
+                              <!-- <h4 class="text-orange" >Monday <span class="text-primary"> Motivation </span></h4> -->
+                              <img src="./storage/uploads/heartday-2.jpg" style="z-index: 2" />
+                               <p class="text-center" style="padding: 30px;"><br/><br/><strong class="text-primary">Sept.29 : Happy World Heart Day!</strong>
+                                <br/><br/>Every heartbeat matters. On #WorldHeartDay, make the commitment to take care of your heart and prevent heart diseases and stroke. Inspire your loved ones to do the same!<br/><br/>#WeSpeakYourLanguage #OAonHeartDay</p>
+
+                              <br/><br/>
+
+                  </div>
+
 
 <!-- SUICIDE -->
 <div class="item text-center" style="background-color: #fff" >
