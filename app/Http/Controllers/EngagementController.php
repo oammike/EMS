fbae1b1c73850cd00e05e52da76cd84bf7add475
Mike <mpamero@openaccessbpo.com>
2019-10-09 13:07:57 +0800

    commit to checkout master

--- app/Http/Controllers/EngagementController.php
+++ app/Http/Controllers/EngagementController.php
@@ -61,6 +61,7 @@ class EngagementController extends Controller
     							where('user_id',$this->user->id)->
                                 join('engagement_entryDetails','engagement_entryDetails.engagement_entryID','=','engagement_entry.id')->
                                 join('engagement_entryItems','engagement_entryDetails.entry_itemID','=','engagement_entryItems.id')->get();
+        
     							//select('id')->get();
     	(count($existingEntry) > 0) ? $hasEntry=true : $hasEntry=false;
     	//return $existingEntry;
@@ -68,6 +69,7 @@ class EngagementController extends Controller
     	return view('people.empEngagement-show',compact('engagement','id','hasEntry','existingEntry'));
     	//return $engagement;
     }
+
 
     public function saveEntry(Request $request)
     {
@@ -95,6 +97,8 @@ class EngagementController extends Controller
     	return response()->json(['success'=>1, 'entry'=>$entry]);
     }
 
+
+
     public function voteNow($id)
     {
         DB::connection()->disableQueryLog(); 
@@ -103,12 +107,21 @@ class EngagementController extends Controller
                             join('engagement_elements','engagement_entryItems.element_id','=','engagement_elements.id')->
                             select('engagement.id','engagement.name as activity','engagement.startDate','engagement.endDate','engagement.body as content','engagement.withVoting','engagement.fairVoting','engagement_entryItems.label','engagement_elements.label as dataType','engagement_entryItems.ordering','engagement_entryItems.id as itemID')->
                             get();
+
         $allEntries = DB::table('engagement_entry')->where('engagement_entry.engagement_id',$id)->
                                 join('engagement','engagement_entry.engagement_id','=','engagement.id')->
                                 join('engagement_entryDetails','engagement_entryDetails.engagement_entryID','=','engagement_entry.id')->
-                                join('engagement_entryItems','engagement_entryDetails.entry_itemID','=','engagement_entryItems.id')->get();
-
-        return $allEntries;
-        return view('people.empEngagement-vote',compact('engagement'));
+                                join('engagement_entryItems','engagement_entryDetails.entry_itemID','=','engagement_entryItems.id')->
+                                join('engagement_elements','engagement_entryItems.element_id','=','engagement_elements.id')->
+                                join('users','engagement_entry.user_id','=','users.id')->
+                                join('team','team.user_id','=','users.id')->
+                                join('campaign','team.campaign_id','=','campaign.id')->
+                                join('positions','users.position_id','=','positions.id')->
+                                select('engagement.name as activity','engagement_entry.id as entryID', 'engagement_entryItems.ordering', 'engagement_entryDetails.value as value','engagement_elements.label as elemType','engagement_entryItems.label','engagement_entry.user_id','users.firstname','users.lastname','users.nickname','positions.name as jobTitle' ,'campaign.name as program','engagement_entry.created_at')->get();
+        $userEntries = collect($allEntries)->groupBy('entryID');
+        
+        //return $allEntries;
+        
+        return view('people.empEngagement-vote',compact('engagement','allEntries','id','userEntries'));
     }
 }
--- resources/views/people/empEngagement-vote.blade.php
+++ resources/views/people/empEngagement-vote.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('metatags')
-<title>All Entries: Employee Engagement Activities | EMS</title>
+<title>All Entries: {{$allEntries[0]->activity}}| EMS</title>
 @endsection
 
 @section('content')
@@ -12,8 +12,8 @@
       <h1>&nbsp;</h1>
       <ol class="breadcrumb">
         <li><a href="{{action('HomeController@index')}}"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="{{action('UserController@index')}}"> All Employees</a></li>
-        <li class="active">My Team</li>
+        <li><a href="{{action('EngagementController@show',$id)}}"> Employee Engagement Activities</a></li>
+        <li class="active">{{$allEntries[0]->activity}}</li>
       </ol>
     </section>
 
@@ -23,6 +23,76 @@
              <div class="col-lg-12">
 
               <div class="box box-primary"  style="background: rgba(256, 256, 256, 0.4);padding:30px">
+
+                <h3 class="text-default"><span style="font-weight: 100"> All Entries:</span> <a href="{{action('EngagementController@show',$id)}}"> {{$allEntries[0]->activity}}</a><br/><br/></h3> 
+
+                <?php $ctr=1;?>
+
+                @foreach($userEntries as $entry)
+
+               
+
+                <div style="width: 45%;margin-right:20px" class="pull-left">
+                  <!-- ******** collapsible box ********** -->
+                  <div class="box box-default collapsed-box">
+                    <div class="box-header with-border">
+                      <h3 class="box-title text-primary">{{$entry[0]->value}}  <small id=" "><i class="fa fa-exclamation-circle text-yellow"></i></small> </h3>
+
+
+
+                      <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+                        </button>
+                      </div>
+                      <!-- /.box-tools -->
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                      @foreach($entry as $e)
+
+                      
+
+                        @if($e->elemType === 'PAR')
+                          <h4>{{$e->label}} : </h4>   <div style="margin:20px; white-space: pre-wrap;">{!! $e->value !!}</div>
+                        @else
+                           <h4>{{$e->label}} :  <span style="font-weight: 100"> {!! $e->value !!}</span></h4> 
+
+                        @endif
+
+                        
+
+                      @endforeach
+                      <div class="clearfix"></div>
+                      <p class="pull-right" style="width: 50%">
+                          
+                          <a href="{{action('UserController@show',$e->user_id)}}" target="_blank"><img class="user-image pull-right" width="80" src="../../public/img/employees/{{$e->user_id}}.jpg">
+                          @if(empty($e->nickname))
+                            <strong>{{$e->firstname}} {{$e->lastname}}<br/></strong>
+                          
+                          @else
+                            <strong>{{$e->nickname}} {{$e->lastname}}<br/></strong>
+                          @endif
+                        </a>
+                          
+                          <small>{{$e->jobTitle}}</small> - {{$e->program}}
+                      </p>
+
+                      
+                    </div>
+                    <!-- /.box-body -->
+                  </div>
+                  <!-- ******** end collapsible box ********** -->
+                </div>
+                @if ( $ctr % 2  == 0 )
+                <div class="clearfix">&nbsp;</div>
+                @endif
+                <?php $ctr++; ?>
+
+
+
+                @endforeach
+
+                <div class="clearfix">&nbsp;</div>
                 
 
 
@@ -59,47 +129,7 @@
   $(function () {
    'use strict';
 
-   $('#submit').on('click',function(){
-
-    var items = $('#entry .form-control').map(function( i, e ) {
-                  return $( e ).val();
-                }).get();
-    var itemIDs = $('#entry .form-control').map(function( i, e ) {
-                  return $( e ).attr('data-itemID');
-                }).get();
-    var _token = "{{ csrf_token() }}";
-
-    $.ajax({
-
-                url:"{{action('EngagementController@saveEntry')}}",
-                type:'POST',
-                data:{
-
-                  'engagement_id': "{{$id}}",
-                  'items': items,
-                  'itemIDs': itemIDs,
-                  
-                  _token: _token
-
-                },
-                error: function(response)
-                { console.log("Error saving entry: ");
-                  console.log(response);
-                  $.notify("Error processing request. Please check all submitted fields and try again.",{className:"error", globalPosition:'right middle',autoHideDelay:7000, clickToHide:true} ); 
-                  return false;
-                },
-                success: function(response)
-                {
-                  console.log(response);
-                  $.notify("Entry submitted. \nThank you for participating.",{className:"success", globalPosition:'right middle',autoHideDelay:7000, clickToHide:true} );
-                  $('#submit').fadeOut();
-
-                }
-
-          });
-
-   });
-
+   
   
        
         
