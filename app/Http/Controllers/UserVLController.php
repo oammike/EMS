c3f4c638817f5075db672bf8cb66ac6f4bfed17a
Mike <mpamero@openaccessbpo.com>
2020-02-23 12:57:08 +0800

    changed log file

--- app/Http/Controllers/UserVLController.php
+++ app/Http/Controllers/UserVLController.php
@@ -159,7 +159,7 @@ class UserVLController extends Controller
         $correct = Carbon::now('GMT+8'); //->timezoneName();
 
                        if($this->user->id !== 564 ) {
-                          $file = fopen('public/build/changes.txt', 'a') or die("Unable to open logs");
+                          $file = fopen('public/build/rewards.txt', 'a') or die("Unable to open logs");
                             fwrite($file, "-------------------\n Tried [VL]: ".$user->lastname."[".$user->id."] --" . $correct->format('M d h:i A'). " by [". $this->user->id."] ".$this->user->lastname."\n");
                             fclose($file);
                         } 
@@ -533,7 +533,7 @@ class UserVLController extends Controller
     {
 
         /* -------------- log updates made --------------------- */
-        $file = fopen('public/build/changes.txt', 'a') or die("Unable to open logs");
+        $file = fopen('public/build/rewards.txt', 'a') or die("Unable to open logs");
         fwrite($file, "-------------------\n [". $request->id."] VL REQUEST \n");
         fclose($file);
 
@@ -576,7 +576,7 @@ class UserVLController extends Controller
         $allUserNotifs = User_Notification::where('notification_id',$theNotif->id)->delete(); */
         
           /* -------------- log updates made --------------------- */
-         $file = fopen('public/build/changes.txt', 'a') or die("Unable to open logs");
+         $file = fopen('public/build/rewards.txt', 'a') or die("Unable to open logs");
             fwrite($file, "-------------------\n [". $vl->id."] VL update ". date('M d h:i:s'). " by [". $this->user->id."], ".$this->user->lastname."\n");
             fclose($file);
 
@@ -719,7 +719,7 @@ class UserVLController extends Controller
                         $m->to($TL->userData->email, $TL->lastname.", ".$TL->firstname)->subject('New CWS request');     
 
                         
-                             $file = fopen('public/build/changes.txt', 'a') or die("Unable to open logs");
+                             $file = fopen('public/build/rewards.txt', 'a') or die("Unable to open logs");
                                 fwrite($file, "-------------------\n Email sent to ". $TL->userData->email."\n");
                                 fclose($file);                      
                     
@@ -766,7 +766,7 @@ class UserVLController extends Controller
 
 
          /* -------------- log updates made --------------------- */
-         $file = fopen('public/build/changes.txt', 'a') or die("Unable to open logs");
+         $file = fopen('public/build/rewards.txt', 'a') or die("Unable to open logs");
             fwrite($file, "-------------------\n". $employee->id .",". $employee->lastname." VL submission ". date('M d h:i:s'). " by ". $this->user->firstname.", ".$this->user->lastname."\n");
             fclose($file);
          
@@ -855,7 +855,7 @@ class UserVLController extends Controller
         $correct = Carbon::now('GMT+8'); //->timezoneName();
 
        if($this->user->id !== 564 ) {
-          $file = fopen('public/build/changes.txt', 'a') or die("Unable to open logs");
+          $file = fopen('public/build/rewards.txt', 'a') or die("Unable to open logs");
             fwrite($file, "-------------------\n CheckVLcredits: --" . $correct->format('M d h:i A'). " by [". $this->user->id."] ".$this->user->lastname."\n");
             fclose($file);
         } 
