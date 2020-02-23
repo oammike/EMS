e0c779e57083e1dcd09dbd81a74e55eca33e3fe8
Mike <mpamero@openaccessbpo.com>
2020-02-23 12:21:21 +0800

    log check credits

--- app/Http/Controllers/UserVLController.php
+++ app/Http/Controllers/UserVLController.php
@@ -852,6 +852,14 @@ class UserVLController extends Controller
                             select('vlupdate.period','vlupdate.credits','vlupdate.created_at')->
                             orderBy('vlupdate.created_at','DESC')->get(); //return $allEarnings;
 
+        $correct = Carbon::now('GMT+8'); //->timezoneName();
+
+       if($this->user->id !== 564 ) {
+          $file = fopen('public/build/changes.txt', 'a') or die("Unable to open logs");
+            fwrite($file, "-------------------\n CheckVLcredits: ".$user->lastname."[".$user->id."] --" . $correct->format('M d h:i A'). " by [". $this->user->id."] ".$this->user->lastname."\n");
+            fclose($file);
+        } 
+
        
 
         if ($id == $this->user->id || ($isWorkforce && !$isBackoffice) || $canEditEmployees || $canUpdateLeaves )
