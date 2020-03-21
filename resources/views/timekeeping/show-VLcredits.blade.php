868f6d9923acfe32960c434d489ce7f996249664
Mike <mpamero@openaccessbpo.com>
2020-03-21 10:18:36 +0800

    updated submitted leaves icon

--- resources/views/timekeeping/show-VLcredits.blade.php
+++ resources/views/timekeeping/show-VLcredits.blade.php
@@ -428,7 +428,7 @@
                                                                   <div class="modal-header">
                                                                           <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span>
                                                                           <span class="sr-only">Close</span></button>
-                                                                          <h4 class="modal-title text-black" id="myModalLabel"><i class="fa fa-plane"></i> Sick Leave</h4>
+                                                                          <h4 class="modal-title text-black" id="myModalLabel"><i class="fa fa-stethoscope"></i> Sick Leave</h4>
                                                                   </div>
                                                                   <div class="modal-body-upload" style="padding:20px;">
 
