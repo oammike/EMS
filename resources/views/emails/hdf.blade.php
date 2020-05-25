276fe16c2d564853c19217bb73da552d05c3af36
Mike <mpamero@openaccessbpo.com>
2020-05-25 21:33:57 +0800

    email HDF

--- resources/views/emails/hdf.blade.php
+++ resources/views/emails/hdf.blade.php
@@ -3,13 +3,13 @@
 	<br/>Health Declaration Alert</h2>
 
 
-<p>New Health Form Declaration Alert from: <strong> @if(is_null($employee->nickname)) {{$employee->firstname}} @else {{$employee->nickname}} @endif {{$employee->lastname}} </strong> <br/><br/>
+<p>New Health Form Declaration Alert from: <strong> @if(is_null($employee->nickname)) {{$employee->firstname}} @else {{$employee->nickname}} @endif {{$employee->lastname}} [ {{$program->name}} ]</strong> <br/><br/>
 
 	
-   ------------------------------------------------------------<br/>
+   ------------------------------------------------------------<br/><br/>
 
 	In line with our Health and Safety Protocols, please be informed that {{$employee->firstname}} {{$employee->lastname}} is required to stay at home.  <br/><br/>
-	Our Nurses will advise you if{{$employee->firstname}} is cleared to return to work in the office. 
+	Our Nurses will advise you if{{$employee->firstname}} is cleared to return to work in the office. <br/><br/>
 	
 	
    ------------------------------------------------------------<br/><br/><br/>
