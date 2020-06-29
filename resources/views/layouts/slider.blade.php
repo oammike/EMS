72b0e307ff8dc0229ab9602db9b7b14083b318ed
Mike <mpamero@openaccessbpo.com>
2020-06-29 11:51:31 +0800

    week 8-9

--- resources/views/layouts/slider.blade.php
+++ resources/views/layouts/slider.blade.php
@@ -21,6 +21,17 @@
  
                     
               </div>
+               <div class="item text-center" >
+                  <h4 class="text-orange" style="line-height: 1.5em" >Stay Connected:<br/><span class="text-primary">ECQonnection Wall<br/>
+                  
+                    <img src="storage/uploads/divider.png" />
+                    </h4>
+
+                    <img src="storage/uploads/wall_week8.jpg" width="100%" /><br/>
+                    <a class="btn btn-success btn-md" href="{{action('EngagementController@show',15)}}"> Check out our ECQonnection Wall</a>
+                    
+                    
+              </div>
 
               <div class="item text-center">
                 <img src="storage/uploads/oneforhealth_8.jpg" width="100%" />
@@ -175,17 +186,7 @@
  
                     
               </div>
-              <div class="item text-center" >
-                  <h4 class="text-orange" style="line-height: 1.5em" >Stay Connected:<br/><span class="text-primary">ECQonnection Wall<br/>
-                  
-                    <img src="storage/uploads/divider.png" />
-                    </h4>
-
-                    <img src="storage/uploads/wall_week7.jpg" width="100%" /><br/>
-                    <a class="btn btn-success btn-md" href="{{action('EngagementController@show',14)}}"> Check out our ECQonnection Wall</a>
-                    
-                    
-              </div>
+             
 
               <div class="item text-center">
                 <img src="storage/uploads/oneforhealth_5.jpg" width="100%" />
