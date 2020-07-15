c964b16edfc1342574c8e20d82731b1cc93a69f1
Mike <mpamero@openaccessbpo.com>
2020-07-15 20:51:29 +0800

    removed Lisa from orgchart

--- resources/views/people/campaigns-orgChart.blade.php
+++ resources/views/people/campaigns-orgChart.blade.php
@@ -26,7 +26,7 @@
               <div class="box box-primary"  style="background: #fff;"><!-- ; rgba(256, 256, 256, 1) -->
                       <div class="box-header ">
                         <h4 class="text-blue pull-left"><i class="fa fa-users"></i><strong> Your Entire Team </strong> </h4>
-                        <img src="storage/uploads/exec.jpg" width="75%" class="pull-right" />
+                        <img src="{{asset('storage/uploads/newexecs.jpg')}}" width="75%" class="pull-right" />
                         <div style="font-size: smaller; width: 35%;margin:30px 0 0 40px; position: absolute;left:10px; top:95px">
                           <i class="fa fa-info-circle text-primary"></i> Click on an employee node to view and expand the tree. <br/>
                           <i class="fa fa-info-circle text-primary"></i> Shortcut buttons to MOVEMENT, PROFILE, DTR, and SCHEDULE <br/>appear on the right side of employee node<br/>
@@ -61,16 +61,7 @@
               </div><!--end box-primary--><br/><br/>
 
 
-              <div class="box box-primary"  style="background: rgba(256, 556, 256, 0.4)">
-                <div class="box-header ">
-                        <h5 class="text-blue pull-left"><i class="fa fa-user"></i><strong> {{$user3[0]->position}} : </strong> {{$user3[0]->nickname}} {{$user3[0]->lastname}}  </h5>
-                      </div>
-
-                      <div class="box-body">
-                        <div id="basicdiagram3" style="width: 100%; height: 350px; border-style: dotted; border-width: 1px; overflow-x: scroll;"></div>
-                        <br/><br/>
-                      </div>   
-              </div><!--end box-primary--><br/><br/>
+              
 
 
               <div class="box box-primary"  style="background: rgba(256, 256, 256, 0.4)">
