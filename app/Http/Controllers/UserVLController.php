7656b2db56ba6f689184cb0f72e7ec93cfbf88cf
Mike <mpamero@openaccessbpo.com>
2020-07-28 15:26:57 +0800

    debugging vl

--- app/Http/Controllers/UserVLController.php
+++ app/Http/Controllers/UserVLController.php
@@ -297,7 +297,7 @@ class UserVLController extends Controller
                                 $schedForTheDay = $this->getWorkSchedForTheDay1($user,$vl_from,null,false);
 
                                  if ($this->user->id == 3323)
-                                    return response()->json(['isRD'=>$schedForTheDay->isRD]);
+                                    return response()->json(['isRD'=>$schedForTheDay[0]]);
 
                                 //if 4HRs lang work nya, part timer sya or foreign na part timer
                                 //dapat half lang credit nila
