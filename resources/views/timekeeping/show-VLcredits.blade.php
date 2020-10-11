55fe72134e62ac5c6188d59e363bbffebc1e9529
Mike <mpamero@openaccessbpo.com>
2020-10-11 09:29:02 +0800

    as of cutoff instead earning

--- resources/views/timekeeping/show-VLcredits.blade.php
+++ resources/views/timekeeping/show-VLcredits.blade.php
@@ -378,7 +378,7 @@
                                         </td>
                                         <td class="text-center @if($ctr==1) text-success" style="font-size: larger; font-weight: bold; @endif">
                                           {{ number_format( (($v->beginBalance - $v->used)-$v->paid)+ $earn - $earnVTO, 2) }}
-                                          @if ($v->creditYear == date('Y'))<br/><small style="font-weight: normal;">as of ({{date('M d,Y',strtotime($allEarnings[0]->period))}}) <br/>leave earnings </small>@endif
+                                          @if ($v->creditYear == date('Y'))<br/><small style="font-weight: normal;">as of ({{date('M d,Y',strtotime($allEarnings[0]->period))}}) <br/>cutoff </small>@endif
                                         </td>
                                         <td class="text-center">
                                          
