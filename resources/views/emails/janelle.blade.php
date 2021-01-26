dcd4700d56c14a16497174011ce135ce2abf5e8d
Mike <mpamero@openaccessbpo.com>
2021-01-26 08:54:18 +0800

    update janelle template

--- resources/views/emails/janelle.blade.php
+++ resources/views/emails/janelle.blade.php
@@ -3,13 +3,13 @@
 
 <p style="font-family: Arial,sans-serif; color:#333;">Hi Janelle,<br/><br/>Here are our {{$greet}} celebrators for this period:</p>
 
-<table border="1" style="font-family: Arial,sans-serif; color:#333;">
+<table border="1" style="font-family: Arial,sans-serif; color:#333;" cellpadding="5">
 	<thead>
 		<tr>
 			@if($type == '1')
 			<th>Employee</th><th>Program</th><th>Birthday</th><th>Tenure</th>
 			@else
-			<th>Employee</th><th>Program</th><th>Tenure</th><th>Work Anniversary</th>
+			<th>Employee</th><th>Program</th><th>Work Anniversary</th><th>Tenure</th>
 			@endif
 		</tr>
 	</thead>
