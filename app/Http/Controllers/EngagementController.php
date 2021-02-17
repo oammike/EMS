0d92eb660e8073798fc24573d86f5a543ca8b585
Mike <mpamero@openaccessbpo.com>
2021-02-17 16:18:13 +0800

    test writing to home/EMS

--- app/Http/Controllers/EngagementController.php
+++ app/Http/Controllers/EngagementController.php
@@ -795,10 +795,10 @@ class EngagementController extends Controller
                         $image->move($destinationPath, $new_name);
 
                         //copy to new /home/EMS location
-                        /*$new_name2 = $filen.$this->user->id."_".rand() .'.' . $image2->getClientOriginalExtension();
+                        $new_name2 = $filen.$this->user->id."_".rand() .'.' . $image2->getClientOriginalExtension();
                         $storagePath = '/home/EMS/public/storage';
                         $destinationPath2 = $storagePath . '/uploads/';
-                        $image2->move($destinationPath2, $new_name2);*/
+                        $image2->move($destinationPath2, $new_name2);
 
 
 
