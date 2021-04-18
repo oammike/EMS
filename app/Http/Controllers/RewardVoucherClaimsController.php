068483667c91a0e5bef5766f3a507f4cf5ec957b
unknown <lockzackary@gmail.com>
2021-04-19 00:25:07 +0800

    - fixed bug with display of Voucher Claims (wrong count passed to data table) - added export function to Voucher Claims with Statistics on Total Unused Points, Total Points Earned, Total Voucher Claims and breakdown for each month

--- app/Http/Controllers/RewardVoucherClaimsController.php
+++ app/Http/Controllers/RewardVoucherClaimsController.php
@@ -55,22 +55,22 @@ class RewardVoucherClaimsController extends Controller
             LEFT JOIN users on voucher_claims.user_id = users.id
             LEFT JOIN user_leaders on user_leaders.user_id = users.id
             LEFT JOIN immediateHead_Campaigns on user_leaders.immediateHead_Campaigns_id = immediateHead_Campaigns.id
-            LEFT JOIN campaign on immediateHead_Campaigns.campaign_id = campaign.id                    
+            LEFT JOIN campaign on immediateHead_Campaigns.campaign_id = campaign.id
             LEFT JOIN vouchers on voucher_claims.voucher_id = vouchers.id
           GROUP BY vid
           ORDER BY voucher_claims.created_at asc
-                    
+
         "));
     $callback = function() use ($orders) {
         $file = fopen('php://output', 'w');
-        
+
         $columns = ["Lastname","Firstname","Nickname","Campaign","Voucher","Email","Phone","Claimed","Denied","Date Requested","Date Processed"];
         fputcsv($file, $columns);
         foreach($orders as $order){
           $csvLine = [];
-          $csvLine[] = $order->last; 
-          $csvLine[] = $order->first; 
-          $csvLine[] = $order->nick; 
+          $csvLine[] = $order->last;
+          $csvLine[] = $order->first;
+          $csvLine[] = $order->nick;
           $csvLine[] = $order->campaign_name;
           $csvLine[] = $order->vname;
           $csvLine[] = $order->vemail;
@@ -79,10 +79,137 @@ class RewardVoucherClaimsController extends Controller
           $csvLine[] = ($order->denied==1) ? "YES" : "NO" ;
           $csvLine[] = $order->date_requested;
           $csvLine[] = $order->date_processed;
-          fputcsv($file, $csvLine);          
-        }        
+          fputcsv($file, $csvLine);
+        }
         fclose($file);
-      };   
+      };
+
+    return Response::stream($callback, 200, $headers);
+  }
+
+  public function export_voucher_stats(Request $request){
+    $reference_date = new \DateTime('NOW');
+    $dt = new \DateTime($reference_date->format('Y-m-d'));
+    $firstday = $dt->format('Y-01-01 00:00:00');
+    $lastday = $dt->format('Y-m-t 23:59:59');
+    $endtimestamp = strtotime($lastday);
+    $starttimestamp = strtotime($firstday);
+
+
+    $awards = \DB::select(\DB::raw("
+          SELECT
+            user_id, points, MONTH(created_at) AS `month`
+          FROM reward_award
+          WHERE
+            created_at > FROM_UNIXTIME(".$starttimestamp.")
+            AND created_at < FROM_UNIXTIME(".$endtimestamp.")
+          ORDER BY user_id ASC, created_at ASC
+        "));
+
+
+    $users = \DB::select(\DB::raw("
+          SELECT
+            users.id as userid, UPPER(users.firstname) as 'first', UPPER(users.lastname) as 'last',
+            campaign.id as 'cid', campaign.name as 'program',
+            points.points as 'unused_points'
+
+          FROM
+            users
+            LEFT JOIN user_leaders on user_leaders.user_id = users.id
+            LEFT JOIN immediateHead_Campaigns on user_leaders.immediateHead_Campaigns_id = immediateHead_Campaigns.id
+            LEFT JOIN campaign on immediateHead_Campaigns.campaign_id = campaign.id
+            LEFT JOIN points on users.id = points.idnumber
+          WHERE users.status_id not in (7, 8, 9, 16)
+          ORDER BY users.id asc
+        "));
+
+    $claims_raw = \DB::select(\DB::raw("
+          SELECT
+            voucher_claims.user_id, vouchers.cost, MONTH( voucher_claims.created_at ) AS `month`
+          FROM
+            voucher_claims
+            LEFT JOIN vouchers on voucher_claims.voucher_id = vouchers.id
+          WHERE voucher_claims.created_at > FROM_UNIXTIME(". $starttimestamp .") AND voucher_claims.created_at < FROM_UNIXTIME(".$endtimestamp.")
+          ORDER BY voucher_claims.user_id ASC, voucher_claims.created_at ASC
+    "));
+
+    $earnings = [];
+    $max_month = 0;
+    foreach($awards as $award){
+      $earnings[$award->user_id][$award->month] = $award->points;
+      if(intval($award->month)>$max_month){ $max_month = intval($award->month); }
+    }
+
+    $claims = [];
+    foreach($claims_raw as $claim){
+      $claims[$claim->user_id][$claim->month] = $claim->cost;
+    }
+
+
+    $headers = array(
+        "Content-type" => "text/csv",
+        "Content-Disposition" => "attachment; filename=Reward_Voucher_Points_Report.csv",
+        "Pragma" => "no-cache",
+        "Cache-Control" => "must-revalidate, post-check=0, pre-check=0",
+        "Expires" => "0"
+      );
+
+    $callback = function() use ($users,$earnings,$claims,$max_month) {
+        $months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEPT", "OCT", "NOV", "DEC"];
+        $file = fopen('php://output', 'w');
+
+        $columns = ["EMS ID","FIRSTNAME","LASTNAME","PROGRAM","Total Unused Points"];
+        $i = 0;
+        while($i < $max_month){
+          $columns[] = $months[$i] . " earnings";
+          $i++;
+        }
+        $columns[] = "Total Points Earned";
+        $i = 0;
+        while($i < $max_month){
+          $columns[] = $months[$i] . " Voucher Claims";
+          $i++;
+        }
+        $columns[] = "Total Voucher Claims";
+
+        fputcsv($file, $columns);
+        /*
+        users.id as userid, users.employeeNumber as 'employee_number', UPPER(users.firstname) as 'first', UPPER(users.lastname) as 'last',
+            campaign.id as 'cid', campaign.name as 'program',
+            points.points as 'unused_points'
+        */
+        foreach($users as $user){
+          $csvLine = [];
+          $csvLine[] = $user->userid;
+          $csvLine[] = $user->first;
+          $csvLine[] = $user->last;
+          $csvLine[] = $user->program;
+          $csvLine[] = $user->unused_points;
+
+          $i = 0;
+          $total = 0;
+          while($i < $max_month){
+            $earned = isset($earnings[$user->userid][$i+1]) ? $earnings[$user->userid][$i+1] : 0;
+            $total = $total + $earned;
+            $csvLine[] = $earned;
+            $i = $i + 1;
+          }
+          $csvLine[] = $total;
+
+          $i = 0;
+          $total = 0;
+          while($i < $max_month){
+            $earned = isset($claims[$user->userid][$i+1]) ? $claims[$user->userid][$i+1] : 0;
+            $total = $total + $earned;
+            $csvLine[] = $earned;
+            $i = $i + 1;
+          }
+          $csvLine[] = $total;
+
+          fputcsv($file, $csvLine);
+        }
+        fclose($file);
+      };
 
     return Response::stream($callback, 200, $headers);
   }
@@ -90,33 +217,34 @@ class RewardVoucherClaimsController extends Controller
   public function list_claims(Request $request,$show_redeemed = FALSE)
   {
       $columns = array('','name','quantity','cost');
-    
+
       $skip = $request->input('start');
       $take = $request->input('length', $this->pagination_items);
-      
+
       $fetch_all = $request->input('fetch_all', FALSE);
-      $data = new \stdClass();      
-      
+      $data = new \stdClass();
+
       //if($show_redeemed ==TRUE){
         //$query = VoucherClaims::with('user','voucher')->where('redeemed','=','1')->orWhere('denied','=','1');
       //}else{
         $query = VoucherClaims::with('user','voucher')->where('redeemed','=',0)->where('denied','=',0);
       //}
-      
+
       $order = $request->input('order.0.column',0);
       if( $order != 0 ){
         $query = $query->orderBy($columns[$order], $request->input('order.0.dir'));
       } else {
         $query = $query->orderBy('created_at', 'asc');
       }
-      
+
       $query->skip($skip)->take($take);
       $data->data = $query->get();
-      $data->iTotalRecords =  VoucherClaims::count();
+      $data->iTotalRecords =  $data->data->count();
+
       $data->recordsFiltered = $data->iTotalRecords;
-      
-    
-      
+
+
+
       return response()->json( $data );
 
 
@@ -132,7 +260,7 @@ class RewardVoucherClaimsController extends Controller
                     LEFT JOIN users on voucher_claims.user_id = users.id
                     LEFT JOIN user_leaders on user_leaders.user_id = users.id
                     LEFT JOIN immediateHead_Campaigns on user_leaders.immediateHead_Campaigns_id = immediateHead_Campaigns.id
-                    LEFT JOIN campaign on immediateHead_Campaigns.campaign_id = campaign.id                    
+                    LEFT JOIN campaign on immediateHead_Campaigns.campaign_id = campaign.id
                     LEFT JOIN vouchers on voucher_claims.voucher_id = vouchers.id
                   WHERE voucher_claims.redeemed = 0
                   ORDER BY voucher_claims.created_at asc
@@ -153,7 +281,7 @@ class RewardVoucherClaimsController extends Controller
       $record->target_id = $user_id;
       $record->description = "Refunded points because the voucher: ".$claim->voucher->name." you tried to claim was denied";
       if($record->save()) {
-        $claim = VoucherClaims::find($reward_id);        
+        $claim = VoucherClaims::find($reward_id);
         $claim->denied = 1;
         if($claim->save()){
           $error = false;
@@ -172,20 +300,20 @@ class RewardVoucherClaimsController extends Controller
 
     if($error){
       return response()->json([
-        'success' => false,                  
+        'success' => false,
         'message' => "could not refund points for " . $user->firstname . " " . $user->lastname
       ], 422);
     }else{
       return response()->json([
-        'success' => true,                  
+        'success' => true,
         'message' => "Voucher claim succesfully denied. ".$user->firstname . " " . $user->lastname. " has been refunded " . $cost . " points"
-      ], 200);    
+      ], 200);
     }
-    
+
   }
 
   public function confirm_claim(Request $request,$reward_id = 0){
-    
+
     $claim = VoucherClaims::find($reward_id);
 
     if($claim){
@@ -199,19 +327,19 @@ class RewardVoucherClaimsController extends Controller
       /*
       $attachment = $request->input('attachment',"");
       Mail::send('emails.voucher',
-        [          
+        [
           'voucher_name'=>$claim->voucher->name,
           'instructions' => $request->input('instructions',"")
         ],
         function ($m) use ($claim, $attachment) {
-          
+
           $m->from('OES@openaccessbpo.net', 'OES-OAMPI Evaluation System');
-          $m->to($claim->user->email, $claim->user->firstname." ".$claim->user->lastname)->subject('OAM Rewards - Voucher Claim');     
+          $m->to($claim->user->email, $claim->user->firstname." ".$claim->user->lastname)->subject('OAM Rewards - Voucher Claim');
           if($attachment!=""){
             $file = Input::file('photo');
             $tempDirectory = storage_path('temp');
             $fileName = $file->getClientOriginalName();
-            $file->move($tempDirectory, $fileName);      
+            $file->move($tempDirectory, $fileName);
             $pathToFile = $tempDirectory . '/' . $fileName;
             $m->attach($pathToFile);
           }
@@ -234,7 +362,7 @@ class RewardVoucherClaimsController extends Controller
       ], 422);
     }
 
-    
+
   }
 
 }
