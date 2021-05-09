8ad618a3f594eb9c7a1b5658866f269883238705
Mike <mpamero@openaccessbpo.com>
2021-05-09 16:44:43 +0800

    prelim trainee payslip

--- resources/views/timekeeping/financeTraineeReport.blade.php
+++ resources/views/timekeeping/financeTraineeReport.blade.php
@@ -192,7 +192,7 @@
                       $('#team').html('');
 
                       
-                      var htmltags="<tr><th>Trainee</th><th>Trainer</th><th class='text-right'>Total Hours</th><th>Total Allowance</th><th></th></tr>";// "<tr>";
+                      var htmltags="<tr><th>Trainee</th><th>Trainer</th><th class='text-right'>Total Hours</th><th class='text-right'>Total Allowance</th><th></th></tr>";// "<tr>";
                       var i=0;
 
                       if (rdata.length > 1)
@@ -207,7 +207,7 @@
                           htmltags += "<tr><td>"+(i+1)+". "+ rdata[i]['lastname']+", "+rdata[i]['firstname']+"<br/><small style='font-weight:normal' class='text-primary'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+rdata[i]['jobTitle']+"</small></td>";
                           htmltags += "<td>"+ rdata[i]['leaderFname']+" "+ rdata[i]['leaderLname'] +"</td>";
                           htmltags += "<td class='text-right'>"+ rdata[i]['workedHours'] +"</td>";
-                          htmltags += "<td class='text-right'> Php "+ (rdata[i]['workedHours']/8)*rdata[i]['rate'] +"</td>";
+                          htmltags += "<td class='text-right'> Php "+ parsefloat((rdata[i]['workedHours']/8)*rdata[i]['rate']).toFixed(2) +"</td>";
                           htmltags += "<td class='text-center'><a target='_blank' href='./user_dtr/"+rdata[i]['id']+"?from="+cutoffstart+"&to="+cutoffend+"'  class='btn btn-xs btn-default'><i class='fa fa-calendar-o'></i> View DTR </a></td></tr>";
                         
 
