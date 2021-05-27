099181e964d4343a4eed310837908dbe242f7047
Mike <mpamero@openaccessbpo.com>
2021-05-27 08:04:21 +0800

    fix agent img dashboard

--- resources/views/dashboard-agent.blade.php
+++ resources/views/dashboard-agent.blade.php
@@ -25,7 +25,9 @@ select:-webkit-autofill:focus {
   transition: background-color #f2fcff ease-in-out 0s;
 }
 .tab-content label {font-weight: normal}
-figure img {width: inherit; margin: 0 auto; display: block; position: relative;}
+figure{width: 100%}
+figure img {width: inherit; margin:0 auto; display: block;position: relative;}
+
 
 </style>
 
